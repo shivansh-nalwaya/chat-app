d3b8d5b16df4f28a8ac888c3c0686ac27534f05e
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2019-02-19 17:08:59 +0530

    Add message model migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_113114) do
+ActiveRecord::Schema.define(version: 2019_02_19_113727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,4 +34,5 @@ ActiveRecord::Schema.define(version: 2019_02_19_113114) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "messages", "users"
 end
