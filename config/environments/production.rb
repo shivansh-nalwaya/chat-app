a2aa722ef2d5077e15d982d5ad1e227b49fbe1fc
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2019-02-21 20:19:19 +0100

    Fix hosts for action cable websocket server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.url = 'ws://chat-r-app.herokuapp.com//cable'
-  config.action_cable.allowed_request_origins = [ 'https://chat-r-app.herokuapp.com/' ]
+  config.action_cable.url = 'wss://chat-r-app.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://chat-r-app.herokuapp.com/', 'http://chat-r-app.herokuapp.com/' ]
 end
